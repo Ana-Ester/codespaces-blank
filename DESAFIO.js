fc9e0833450cd03306568aa18fff0e86268c5697
Ana-Ester <152565947+Ana-Ester@users.noreply.github.com>
2023-12-02 01:44:55 +0000

    first commit

---
+++ DESAFIO.js
@@ -0,0 +1,38 @@
+
+
+console.log("Vamos saber em qual nível você está?")
+let name = "Ana"
+let xp = 2000
+
+if(xp < 1000) {
+  console.log("O herói de nome " + name + " está no nível de FERRO!")
+}
+
+  if((xp >= 1001 ) && (xp <= 2000) ){
+  console.log("O herói de nome " + name + " está no nível de BRONZE!")
+}
+
+    if((xp >= 2001 ) && (xp <= 5000) ){
+  console.log("O herói de nome " + name + " está no nível de PRATA!")
+}
+
+      if((xp >= 5001 ) && (xp <= 7000) ){
+  console.log("O herói de nome " + name + " está no nível de OURO!")
+}
+  
+if((xp >= 7001 ) && (xp <= 8000) ){
+  console.log("O herói de nome " + name + " está no nível de PLATINA!")
+}
+
+if((xp >= 8001 ) && (xp <= 9000) ){
+  console.log("O herói de nome " + name + " está no nível de ASCENDENTE!")
+}
+
+if((xp >= 9001 ) && (xp <= 10000) ){
+  console.log("O herói de nome " + name + " está no nível de IMORTAL!")
+}
+
+if (xp >= 10001) {
+    console.log("O herói de nome " + name + " está no nível de RADIANTE!")
+}
+
